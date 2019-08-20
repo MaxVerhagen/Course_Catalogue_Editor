d98795408cbfaa98c01db316bfdaccb3972814e2
a1724402 <a1724402@student.adelaide.edu.au>
2019-08-20 05:38:25 +0000

    before push

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_14_101606) do
+ActiveRecord::Schema.define(version: 2019_08_19_020446) do
+
+  create_table "psoft_db_bases", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "widgets", force: :cascade do |t|
     t.string "name"
