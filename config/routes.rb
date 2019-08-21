08ac7448b02c393d676e39725524506d6f8392c7
a1719719 <ec2-user@ip-172-31-46-218.us-east-2.compute.internal>
2019-08-21 08:04:43 +0000

    adding start of peoplesoft input box things

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'temp/index' #temporary route for index boxes
   resources :widgets
+  resources :temp #temporary location for index boxes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
