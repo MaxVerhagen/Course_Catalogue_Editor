66657802bca2e163ab73805508f69d235c3dfa3c
A1720458 <a1720458@student.adelaide.edu.au>
2019-09-15 23:03:59 +0930

    Created seed script to populate course model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+course_catalogue_data = File.read(Rails.root.join('lib', 'seeds', 'course_catalogue.csv'), encoding: 'ISO-8859-1')
+
+course_catalogue_table = CSV.parse(course_catalogue_data, headers: true)
+
+course_catalogue_table.each do |row|
+	c = Course.new
+	c.effective_date = Date.parse(row['Eff_Date'])
+	c.admin_course_id = row['Course_ID'].to_i
+	row['Status'] == "I" ? c.status = false : c.status = true
+	c.short_title = row['Short_Title']
+	c.long_title = row["Long_Title"]
+	c.min_units = row["Min_Unit"].to_i
+	c.max_units = row["Max_Unit"].to_i
+	c.progress_units = row["Prgrss_Unt"].to_f
+	c.grading = row["Grading"]
+	row['Last_Crse'] == "N" ? c.last_course  = false : c.last_course  = true
+	c.enrollment_unit_calc = row["Enrl_Unt_Calc"]
+	c.description = row["Descr"]
+
+	c.save
+	puts "#{c.short_title}, #{c.admin_course_id} saved" 
+end
