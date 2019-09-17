ecc1ee0763e1e3536fbc613dd2458f36bd8f1ade
A1720458 <a1720458@student.adelaide.edu.au>
2019-09-17 20:44:40 +0930

    Updates course index view to show information about courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   
   #resources :widgets
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html  
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html 
+	
+	resources :courses
+
+	root to: 'courses#index'
    
 end
