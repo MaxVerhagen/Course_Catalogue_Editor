c7e27418cb3cb0edaf9c504b45cf36e64ccef47a
EC2 Default User <ec2-user@ip-172-31-81-97.ec2.internal>
2019-10-15 07:22:07 +0000

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_15_055137) do
+ActiveRecord::Schema.define(version: 2019_10_15_071526) do
 
   create_table "admins", force: :cascade do |t|
     t.string "username"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 2019_10_15_055137) do
     t.boolean "last_course"
     t.integer "enrollment_unit_calc"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "faculties", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
