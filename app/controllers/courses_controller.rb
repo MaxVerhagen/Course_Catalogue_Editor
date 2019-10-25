e8dc4937b1448c881ef9e7c9e3b5846cd452c747
A1720458 <a1720458@student.adelaide.edu.au>
2019-10-25 11:28:10 +1030

    Adding dynamic course titles

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,7 +2,7 @@ class CoursesController < ApplicationController
 	include Pagy::Backend
 
 	protect_from_forgery prepend: true
-  before_action :set_course, only: [:show, :edit, :update, :destroy]
+  	before_action :set_course, only: [:show, :edit, :update, :destroy]
 
   # GET /courses
   # GET /courses.json
@@ -26,11 +26,12 @@ class CoursesController < ApplicationController
 				@pagy, @courses = pagy(@courses.where("long_title like ?", "#{params[:qt]}%"))
 			end
 		end
-  end
+  	end
 
-  # GET /courses/1
-  # GET /courses/1.json
-  def show
+	# GET /courses/1
+	# GET /courses/1.json
+	def show
+		@course = Course.find params[:id]
 	end
 	
 	# POST /search
@@ -44,7 +45,7 @@ class CoursesController < ApplicationController
 
   # GET /courses/new
   def new
-    @course = Course.new
+	@course = Course.new
   end
 
   # GET /courses/1/edit
@@ -54,53 +55,53 @@ class CoursesController < ApplicationController
   # POST /courses
   # POST /courses.json
   def create
-    params.permit!
-    @course = Course.new(course_params)
+	params.permit!
+	@course = Course.new(course_params)
 
-    respond_to do |format|
-      if @course.save
-        format.html { redirect_to @course, notice: 'Course was successfully created.' }
-        format.json { render :show, status: :created, location: @course }
-      else
-        format.html { render :new }
-        format.json { render json: @course.errors, status: :unprocessable_entity }
-      end
-    end
+	respond_to do |format|
+	  if @course.save
+		format.html { redirect_to @course, notice: 'Course was successfully created.' }
+		format.json { render :show, status: :created, location: @course }
+	  else
+		format.html { render :new }
+		format.json { render json: @course.errors, status: :unprocessable_entity }
+	  end
+	end
   end
 
   # PATCH/PUT /courses/1
   # PATCH/PUT /courses/1.json
   def update
-    params.permit!
-    respond_to do |format|
-      if @course.update(course_params)
-        format.html { redirect_to @course, notice: 'Course was successfully updated.' }
-        format.json { render :show, status: :ok, location: @course }
-      else
-        format.html { render :edit }
-        format.json { render json: @course.errors, status: :unprocessable_entity }
-      end
-    end
+	params.permit!
+	respond_to do |format|
+	  if @course.update(course_params)
+		format.html { redirect_to @course, notice: 'Course was successfully updated.' }
+		format.json { render :show, status: :ok, location: @course }
+	  else
+		format.html { render :edit }
+		format.json { render json: @course.errors, status: :unprocessable_entity }
+	  end
+	end
   end
 
   # DELETE /courses/1
   # DELETE /courses/1.json
   def destroy
-    @course.destroy
-    respond_to do |format|
-      format.html { redirect_to courses_url, notice: 'Course was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+	@course.destroy
+	respond_to do |format|
+	  format.html { redirect_to courses_url, notice: 'Course was successfully destroyed.' }
+	  format.json { head :no_content }
+	end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_course
-      @course = Course.find(params[:id])
-    end
+	# Use callbacks to share common setup or constraints between actions.
+	def set_course
+	  @course = Course.find(params[:id])
+	end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def course_params
-      params.fetch(:course, {})
-    end
+	# Never trust parameters from the scary internet, only allow the white list through.
+	def course_params
+	  params.fetch(:course, {})
+	end
 end
