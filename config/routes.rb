45787b3a7fbed5d2f2dbc9b59469d65d2c6077ae
A1720458 <a1720458@student.adelaide.edu.au>
2019-10-26 22:44:40 +1030

    Basic routing and links

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
   
-  get 'admin/index'
-  get 'admin/login'
-  get 'admin/takecredentials'
-  get 'admin/destroy'
-  #resources :widgets
-	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html 
-	resources :admin
-	controller :admin do
-    get 'login' => :new
-    post 'admin/login' => :takecredentials
-    get 'logout' => :destroy
-  end
+	get 'admin/index'
+	get 'admin/login'
+	get 'admin/takecredentials'
+	get 'admin/destroy'
+	#resources :widgets
+		# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html 
+		resources :admin
+		controller :admin do
+		get 'login' => :new
+		post 'admin/login' => :takecredentials
+		get 'logout' => :destroy
+	end
 	
+	get 'courses/history/:id', to: 'course_histories#show', as: 'course_history'
+
 	resources :courses
 	post "courses/search", to: "courses#search" 
 
