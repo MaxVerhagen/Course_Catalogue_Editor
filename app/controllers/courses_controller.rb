d6109687e300fa5a3292991388bc20099326a722
EC2 Default User <ec2-user@ip-172-31-25-148.us-east-2.compute.internal>
2019-10-26 06:37:03 +0000

    Offering page creation of login and quality of life

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -93,6 +93,11 @@ class CoursesController < ApplicationController
 	  format.json { head :no_content }
 	end
   end
+  
+  def offerings
+  	@course = Course.find params[:id]
+  	@theofferings = Offering.where(admin_course_id: @course.admin_course_id)
+  end
 
   private
 	# Use callbacks to share common setup or constraints between actions.
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   
+  get 'offerings/index'
   get 'admin/index'
   get 'admin/login'
   get 'admin/takecredentials'
   get 'admin/destroy'
+  get 'admin/create'
+  get 'admin/adminlist'
+  get 'admin/erase'
+  get 'courses/:id/offerings' => 'courses#offerings', :as => :offering_courses
   #resources :widgets
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html 
 	resources :admin
@@ -11,7 +16,10 @@ Rails.application.routes.draw do
     get 'login' => :new
     post 'admin/login' => :takecredentials
     get 'logout' => :destroy
+    post 'admin/create' => :a_create
+    post 'admin/erase' => :delete
   end
+  
 	
 	resources :courses
 	post "courses/search", to: "courses#search" 
